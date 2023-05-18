ce1e11a274107a715cdf41cd58a789a17f3e7611
Meshack Korir <meshackkorir100@gmail.com>
2023-05-18 07:43:19 +0300

    looped using while method

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
-#write your code here
+# #write your code here
 
-def countdown
+def countdown(number)
+    number = 1
+    while number < 20
+      puts "#{number} SECOND(S)!"
+      sleep(1) # Pause for one second
+      number += 1
+    end
+    puts "HAPPY NEW YEAR!"
 end
